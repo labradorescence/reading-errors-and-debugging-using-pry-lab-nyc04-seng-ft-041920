fd45da6e2ed1bf98158159429d93bc0295242292
Greem <orangenewyorkcreativeagency@gmail.com>
2020-04-28 19:40:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,6 +1,7 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
+<<<<<<< HEAD
 
   "Hello #{player1} & #{player2}"
 
@@ -11,4 +12,12 @@ end
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
+=======
+  puts "Hello #{player1} & #{player2}"
+end
+
+def play_game(player1, player2)
+
+  puts "#{player1} is better than #{player2}"
+>>>>>>> 656db350a017805c980f7e92fcceaa51e5a4005b
 end
